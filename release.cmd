d9fd7fd982e34a8a1ca7f937a684e739f8de560a
JustOff <Off.Just.Off@gmail.com>
2021-02-22 17:22:04 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=1.5.0
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/lull-the-tabs-.+?\.xpi/download\/%VER%\/lull-the-tabs-%VER%\.xpi/" update.xml
+
+set XPI=lull-the-tabs-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
